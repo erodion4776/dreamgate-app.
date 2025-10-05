895dd7271be6338c3b94a6e059b86f8da5424557
erodion4776 <maelizaenterprises@gmail.com>
2025-10-05 03:07:46 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,13 @@
-﻿import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './MainApp'; // <-- PROBLEM LINE
 import './styles/global.css';
-import App from './App';
-
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
-
 root.render(
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>
 );
